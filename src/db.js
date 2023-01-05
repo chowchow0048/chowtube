ff09371f3a7c221c7dbe076dd6bf6084f7b60be6
chowchow0048 <suess98@naver.com>
2023-01-05 17:51:50 +0900

    mongdb and mongoose

---
+++ src/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect("mongodb://127.0.0.1:27017/chowtube", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB ✅");
+const handleError = (error) => console.log("❗️ DB error", error)
+
+db.on("error", handleError);
+db.once("open", handleOpen);
