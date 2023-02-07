9a69ac57c2a80fabb6c2873fc913549cc9f02611
chowchow0048 <suess98@naver.com>
2023-02-07 18:54:52 +0900

    Create Account
    leared how to make a users DB, create account and save it in the DB. Also learned about hashing for password.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,22 @@
-const fakeUser = {
-    userName: "Tom",
-    userAge: "24",
-    userSex: "Male",
+import User from "../models/User"
+
+export const getJoin = (req, res) => res.render("join", {pageTitle: "Join"});
+export const postJoin = async(req, res) => {
+    const {name, username, email, password, location} = req.body;
+    
+    await User.create({
+        name, username, email, password, location
+    });
+
+    return res.redirect("/login");
+};
+
+export const userHome = (req, res) => res.send("userHome");
+
+export const seeUser = (req, res) => {
+    const userName = req.params;
+
+    res.send(`${userName}`);
 }
 
-export const seeUser = (req, res) => res.send("See User Profile");
-
-export const userHome = (req, res) => res.render("userHome", {pageTitle: `${fakeUser.userName}`, fakeUser});
+export const login = (req, res) => res.send("login");
