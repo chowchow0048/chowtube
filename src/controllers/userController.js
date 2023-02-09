6a6e80c1553334d8c3367a19577d3a2e2367669a
chowchow0048 <suess98@naver.com>
2023-02-09 18:32:10 +0900

    form validation, status code, login
    Learned about validating username and password when joining new, changing status code , and validate username and password when logging in.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,64 @@
 import User from "../models/User"
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", {pageTitle: "Join"});
 export const postJoin = async(req, res) => {
-    const {name, username, email, password, location} = req.body;
-    
-    await User.create({
-        name, username, email, password, location
-    });
+    const {name, username, email, password, password2, location} = req.body;
+    const pageTitle = "Join";
 
-    return res.redirect("/login");
+    if(password != password2){
+        return res.status(400).render("join",{
+            pageTitle,
+            errorMessage: "Password confirmation does not match.",
+        })
+    }
+
+    const exists = await User.exists({ $or: [{username}, {email}]});
+    if(exists){
+        return res.status(400).render("join", {
+            pageTitle,
+            errorMessage: "This username/email is already taken"
+        });
+    }
+
+    try {
+        await User.create({
+            name, username, email, password, password2, location
+        });
+
+        return res.redirect("/login");
+    }catch(error){
+        return res.status(400).render("join",{
+            pageTitle: "Upload Video",
+            errorMessage: error._message,
+        });
+    }
 };
+
+export const getLogin = (req, res) => res.render("login", {pageTitle:"Log in"});
+
+export const postLogin = async (req, res) => {
+    const {username, password} = req.body;
+    const pageTitle = "Log In";
+
+    const user = await User.findOne({username});
+    if(!user){
+        return res.status(400).render("login",{
+            pageTitle,
+            errorMessage: "An account with this username does not exists."
+        });
+    }
+
+    const ok = await bcrypt.compare(password, user.password);
+    if(!ok){
+        return res.status(400).render("login",{
+            pageTitle,
+            errorMessage: "Wrong password."
+        });
+    }
+    console.log("USER IN");
+    res.redirect("/");
+}
 
 export const userHome = (req, res) => res.send("userHome");
 
@@ -18,5 +67,3 @@ export const seeUser = (req, res) => {
 
     res.send(`${userName}`);
 }
-
-export const login = (req, res) => res.send("login");
